7a5ab33e35b17d86d093655a6aa3f74d748d175b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-05-27 12:00:32 +0200

    add testmodule directory which we can use to run pydocmd on during development

---
+++ testmodule/__init__.py
@@ -0,0 +1,28 @@
+
+def mycoolfunction(samplesize, width=32, **options):
+  """
+  This is a pretty cool function.
+
+  # Arguments
+  samplesize (int): Test sample size.
+  width (int): The width.
+  options: Additional options
+
+  # Example
+  ```python
+  # This is a very simple example.
+  mycoolfunction(samplesize, width, **options)
+  ```
+
+  See also: #myothercoolfunction().
+  """
+
+  pass
+
+
+def myothercoolfunction(prettycool):
+  """
+  Don't you think?
+  """
+
+  pass
