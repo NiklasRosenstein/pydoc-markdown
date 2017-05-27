39a21ad1ade25109e10cfd32ceee06fbde6e19db
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-05-27 12:29:42 +0200

    Prepare 2.0.1 release.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def readme():
 
 setup(
     name='pydoc-markdown',
-    version='2.0.0',
+    version='2.0.1',
     description='Create Python API documentation in Markdown format',
     long_description=readme(),
     url='https://github.com/NiklasRosenstein/pydoc-markdown',
