5b77acb84fd13fa824e496020b8b63848a9fb9b4
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-05-25 19:08:18 -0300

    fixed codeblock bug

--- pydocmd/restructuredtext.py
+++ pydocmd/restructuredtext.py
@@ -25,10 +25,13 @@ it to fully markdown compatible markup.
 import re
 
 
-class Preprocessor(object):
+class Preprocessor:
   """
   This class implements the preprocessor for restructured text.
   """
+  def __init__(self, config):
+    self.config = config
+
   def preprocess_section(self, section):
     """
     Preprocessors a given section into it's components.
@@ -43,44 +46,41 @@ class Preprocessor(object):
       if line.startswith("```"):
         in_codeblock = not in_codeblock
 
-      if in_codeblock:
-        lines.append(line)
-        continue
+      if not in_codeblock:
+        match = re.match(r':(?:param|parameter)\s+(\w+)\s*:(.*)?$', line)
+        if match:
+          keyword = 'Arguments'
+          param = match.group(1)
+          text = match.group(2)
+          text = text.strip()
 
-      match = re.match(r':(?:param|parameter)\s+(\w+):(.*)$', line)
-      if match:
-        keyword = 'Arguments'
-        param = match.group(1)
-        text = match.group(2)
-        text = text.strip()
+          component = components.get(keyword, [])
+          component.append('- `{}`: {}'.format(param, text))
+          components[keyword] = component
+          continue
 
-        component = components.get(keyword, [])
-        component.append('- `{}`: {}'.format(param, text))
-        components[keyword] = component
-        continue
+        match = re.match(r':(?:return|returns)\s*:(.*)?$', line)
+        if match:
+          keyword = 'Returns'
+          text = match.group(1)
+          text = text.strip()
 
-      match = re.match(r':(?:return|returns):\s+(.*)$', line)
-      if match:
-        keyword = 'Returns'
-        text = match.group(1)
-        text = text.strip()
+          component = components.get(keyword, [])
+          component.append(text)
+          components[keyword] = component
+          continue
 
-        component = components.get(keyword, [])
-        component.append(text)
-        components[keyword] = component
-        continue
+        match = re.match(':(?:raises|raise)\s+(\w+)\s*:(.*)?$', line)
+        if match:
+          keyword = 'Raises'
+          exception = match.group(1)
+          text = match.group(2)
+          text = text.strip()
 
-      match = re.match(':(?:raises|raise)\s+(\w+):(.*)$', line)
-      if match:
-        keyword = 'Raises'
-        exception = match.group(1)
-        text = match.group(2)
-        text = text.strip()
-
-        component = components.get(keyword, [])
-        component.append('- `{}`: {}'.format(exception, text))
-        components[keyword] = component
-        continue
+          component = components.get(keyword, [])
+          component.append('- `{}`: {}'.format(exception, text))
+          components[keyword] = component
+          continue
 
       if keyword is not None:
         components[keyword].append(line)
