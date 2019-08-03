e9c4fbea06e6b6872949ab26c153d6ea129f0385
Niklas Rosenstein <rosensteinniklas@gmail.com>
2019-08-03 18:51:11 +0200

    allow using the python loader from the command-line

--- src/pydoc_markdown/main.py
+++ src/pydoc_markdown/main.py
@@ -71,13 +71,35 @@ class PydocMarkdown(object):
 def main(argv=None, prog=None):
   parser = argparse.ArgumentParser(prog=prog)
   parser.add_argument('-c', '--config', help='Path to the YAML configuration '
-    'file. Defaults to pydoc-markdown.yml', default='pydoc-markdown.yml')
+    'file. Defaults to pydoc-markdown.yml', default=None)
+  parser.add_argument('--modules', nargs='+', help='One or more module names '
+    'to generate API documentation for. If specified, the configuration file '
+    'will not be read, but instead the default configuration is used.')
+  parser.add_argument('--search-path', nargs='+', help='One or more search '
+    'paths for the Python loader. Only used in combination with --modules')
+  parser.add_argument('--path-to', help='Just a helper, will be removed.')
   args = parser.parse_args(argv)
 
+  if args.path_to:
+    import imp
+    print(imp.find_module(args.path_to)[1])
+    exit()
+
+  if args.config and args.modules:
+    parser.error('--modules cannot be combined with --config')
+
   pydocmd = PydocMarkdown()
-  pydocmd.load_config(args.config)
+  if args.modules:
+    args.config = {'loaders': [{
+      'type': 'python',
+      'modules': args.modules,
+      'search_path': args.search_path
+    }]}
+
+  pydocmd.load_config(args.config or 'pydoc-markdown.yml')
   if not pydocmd.loaders:
     parser.error('no loaders configured')
+
   pydocmd.load_module_graph()
   pydocmd.process()
   pydocmd.render()
