7e52cd957e1ba1ff1019bab48f3e6b2f127fe28a
Niklas Rosenstein <rosensteinniklas@gmail.com>
2019-08-16 01:08:21 +0200

    improve Markdown renderer (render TOC, add more options to configure the signature style)

--- src/pydoc_markdown/contrib/renderers/markdown.py
+++ src/pydoc_markdown/contrib/renderers/markdown.py
@@ -37,12 +37,19 @@ class MarkdownRendererConfig(Object):
   encoding = Field(str, default='utf8')
   html_headings = Field(bool, default=False)
   code_headings = Field(bool, default=True)
+  code_lang = Field(bool, default=True)
   descriptive_class_title = Field(bool, default=True)
   add_method_class_prefix = Field(bool, default=True)
   add_full_prefix = Field(bool, default=False)
   sub_prefix = Field(bool, default=False)
-  signature_below_heading = Field(bool, default=True)
-  signature_in_heading = Field(bool, default=False)
+  signature_code_block = Field(bool, default=True)
+  signature_in_header = Field(bool, default=False)
+  signature_with_def = Field(bool, default=True)
+  signature_class_prefix = Field(bool, default=False)
+  signature_expression_maxlength = Field(int, default=100)
+  render_toc = Field(bool, default=True)
+  render_toc_title = Field(str, default='Table of Contents')
+  toc_maxdepth = Field(int, default=2)
 
   # TODO(nrosenstein): render_toc option
 
@@ -52,14 +59,23 @@ class MarkdownRenderer(object):
 
   CONFIG_CLASS = MarkdownRendererConfig
 
-  def render_object(self, fp, level, obj):
+  def _render_toc(self, fp, level, obj):
+    if level > self.config.toc_maxdepth:
+      return
+    object_id = self._generate_object_id(obj)
+    fp.write('  ' * level + '* [{}](#{})\n'.format(self._escape(obj.name), object_id))
+    for child in obj.members.values():
+      self._render_toc(fp, level + 1, child)
+
+  def _render_object(self, fp, level, obj):
     if self.config.html_headings:
-      heading_template = '<h{}>{{title}}</h{}>'.format(level)
+      object_id = self._generate_object_id(obj)
+      heading_template = '<h{0} id="{1}">{{title}}</h{0}>'.format(level, object_id)
     else:
       heading_template = level * '#' + ' {title}'
-    fp.write(heading_template.format(title=self.get_title(obj)))
+    fp.write(heading_template.format(title=self._get_title(obj)))
     fp.write('\n\n')
-    if self.config.signature_below_heading and (obj.is_class() or obj.is_function()):
+    if self.config.signature_code_block and (obj.is_class() or obj.is_function()):
       if obj.is_class():
         func = obj.members.get('__init__')
         if func and not func.is_function():
@@ -67,29 +83,46 @@ class MarkdownRenderer(object):
       else:
         func = obj
       if func:
-        fp.write('```python\n')
+        fp.write('```{}\n'.format('python' if self.config.code_lang else ''))
         for dec in func.decorators:
           fp.write('@{}{}\n'.format(dec.name, dec.args or ''))
         if func.is_async:
           fp.write('async ')
-        fp.write('def ')
+        if self.config.signature_with_def:
+          fp.write('def ')
+        if self.config.signature_class_prefix and (
+            func.is_function() and func.parent and func.parent.is_class()):
+          fp.write(func.parent.name + '.')
         fp.write(func.signature)
         fp.write('\n```\n\n')
-    if self.config.signature_below_heading and obj.is_data():
-      fp.write('```python\n')
-      fp.write(obj.name + ' = ' + str(obj.expr))
+    if self.config.signature_code_block and obj.is_data():
+      fp.write('```{}\n'.format('python' if self.config.code_lang else ''))
+      expr = str(obj.expr)
+      if len(expr) > self.config.signature_expression_maxlength:
+        expr = expr[:self.config.signature_expression_maxlength] + ' ...'
+      fp.write(obj.name + ' = ' + expr)
       fp.write('\n```\n\n')
     if obj.docstring:
       fp.write(obj.docstring)
       fp.write('\n')
     fp.write('\n')
 
-  def render_recursive(self, fp, level, obj):
-    self.render_object(fp, level, obj)
+  def _render_recursive(self, fp, level, obj):
+    self._render_object(fp, level, obj)
     for member in obj.members.values():
-      self.render_recursive(fp, level+1, member)
+      self._render_recursive(fp, level+1, member)
 
-  def get_title(self, obj):
+  def _render(self, graph, fp):
+    if self.config.render_toc:
+      if self.config.render_toc_title:
+        fp.write('# {}\n\n'.format(self.config.render_toc_title))
+      for m in graph.modules:
+        self._render_toc(fp, 1, m)
+      fp.write('\n')
+    for m in graph.modules:
+      self._render_recursive(fp, 1, m)
+
+  def _get_title(self, obj):
     title = obj.name
     if self.config.add_method_class_prefix and obj.is_method():
       title = obj.parent.name + '.' + title
@@ -97,7 +130,7 @@ class MarkdownRenderer(object):
       title = obj.path()
 
     if obj.is_function():
-      if self.config.signature_in_heading:
+      if self.config.signature_in_header:
         title += '(' + obj.signature_args + ')'
       else:
         title += '()'
@@ -116,13 +149,21 @@ class MarkdownRenderer(object):
       title += ' Objects'
     return title
 
+  def _generate_object_id(self, obj):
+    parts = []
+    while obj:
+      parts.append(obj.name)
+      obj = obj.parent
+    return '.'.join(reversed(parts))
+
+  def _escape(self, s):
+    return s.replace('_', '\\_').replace('*', '\\*')
+
   # Renderer
 
   def render(self, config, graph):
     if self.config.filename is None:
-      for m in graph.modules:
-        self.render_recursive(sys.stdout, 1, m)
+      self._render(graph, sys.stdout)
     else:
       with io.open(self.config.filename, 'w', encoding=self.config.encoding) as fp:
-        for m in graph.modules:
-          self.render_recursive(fp, 1, m)
+        self._render(graph, fp)
