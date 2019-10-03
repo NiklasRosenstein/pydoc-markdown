3eed905829e7fae6dc08c2e188fa1f7f47e134f9
Niklas Rosenstein <rosensteinniklas@gmail.com>
2019-10-03 21:41:42 +0200

    starting to build test cases for Python parser (#89)

---
+++ src/tests/pydoc_markdown/contrib/loaders/test_python.py
@@ -0,0 +1,63 @@
+# -*- coding: utf8 -*-
+# Copyright (c) 2019 Niklas Rosenstein
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+import pytest
+import textwrap
+
+from pydoc_markdown.reflection import Argument, Expression, Function
+from pydoc_markdown.contrib.loaders.python import parse_file
+
+
+class TestParser:
+
+  def setup_method(self, test_method):
+    self.current_test_method = test_method
+
+  def parse(self, code):
+    return parse_file(textwrap.dedent(code), self.current_test_method.__name__)
+
+  def test_parser_function_def(self):
+    module = self.parse('''
+      def fun(project_name, project_type, port=8001):
+        pass
+    ''')
+    assert module.name == 'test_parser_function_def'
+    assert len(module.members) == 1
+
+    fun = module.members['fun']
+    assert type(fun) is Function
+    assert fun.name == 'fun'
+    assert len(fun.args) == 3
+    assert fun.args == [
+      Argument('project_name', None, None, Argument.POS),
+      Argument('project_type', None, None, Argument.POS),
+      Argument('port', None, Expression('8001'), Argument.POS),
+    ]
+
+  @pytest.mark.skip
+  def test_parser_function_def_annotations(self):
+    module = self.parse('''
+      def fun(project_name: str, project_type: ProjectType, port: int) -> bool:
+        pass
+    ''')
+
+    # TODO (NiklasRosenstein): This test fails
+    pass
