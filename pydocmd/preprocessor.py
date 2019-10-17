58fa05ec5e7c2b9be46569dd3420e3230aa96a5e
omgpewpew <omgpewpew11@gmail.com>
2019-10-17 20:38:02 +0200

    Change preprocessor to google

--- pydocmd/preprocessor.py
+++ pydocmd/preprocessor.py
@@ -1,85 +1,114 @@
-# Copyright (c) 2017  Niklas Rosenstein
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-"""
-This module implements preprocessing Markdown-like docstrings and converts
-it to fully markdown compatible markup.
-"""
-
 import re
 
 
-class Preprocessor(object):
+class Preprocessor:
   """
-  This class implements the basic preprocessing.
-  """
+  This class implements the preprocessor for Google and PEP 257 docstrings.
 
-  def __init__(self, config):
+  https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+  https://www.python.org/dev/peps/pep-0257/
+  """
+  _param_res = [
+    re.compile(r'^(?P<param>\S+):\s+(?P<desc>.+)$'),
+    re.compile(r'^(?P<param>\S+)\s+\((?P<type>[^)]+)\):\s+(?P<desc>.+)$'),
+    re.compile(r'^(?P<param>\S+)\s+--\s+(?P<desc>.+)$'),
+    re.compile(
+      r'^(?P<param>\S+)\s+\{\[(?P<type>\S+)\]\}\s+--\s+(?P<desc>.+)$'),
+    re.compile(
+      r'^(?P<param>\S+)\s+\{(?P<type>\S+)\}\s+--\s+(?P<desc>.+)$'),
+  ]
+
+  _keywords_map = {
+    'Args:': 'Arguments',
+    'Arguments:': 'Arguments',
+    'Attributes:': 'Attributes',
+    'Choices:': 'Choices',
+    'Example:': 'Examples',
+    'Examples:': 'Examples',
+    'Keyword Args:': 'Arguments',
+    'Keyword Arguments:': 'Arguments',
+    'Methods:': 'Methods',
+    'Note:': 'Notes',
+    'Notes:': 'Notes',
+    'Other Parameters:': 'Arguments',
+    'Parameters:': 'Arguments',
+    'Return:': 'Returns',
+    'Returns:': 'Returns',
+    'Raises:': 'Raises',
+    'References:': 'References',
+    'See Also:': 'See Also',
+    'Todo:': 'Todo',
+    'Warning:': 'Warnings',
+    'Warnings:': 'Warnings',
+    'Warns:': 'Warns',
+    'Yield:': 'Yields',
+    'Yields:': 'Yields',
+  }
+
+  def __init__(self, config=None):
     self.config = config
+
+  def get_section_names(self):
+    return list(self._keywords_map.keys())
 
   def preprocess_section(self, section):
     """
-    Preprocess the contents of *section*.
+    Preprocessors a given section into it's components.
     """
     lines = []
-    codeblock_opened = False
-    current_section = None
+    in_codeblock = False
+    keyword = None
+    components = {}
+
     for line in section.content.split('\n'):
+      line = line.strip()
+
       if line.startswith("```"):
-        codeblock_opened = (not codeblock_opened)
-      if not codeblock_opened:
-        line, current_section = self._preprocess_line(line, current_section)
-      lines.append(line)
-    section.content = self._preprocess_refs('\n'.join(lines))
+        in_codeblock = not in_codeblock
 
-  def _preprocess_line(self, line, current_section):
-    match = re.match(r'# (.*)$', line)
-    if match:
-      current_section = match.group(1).strip().lower()
-      line = re.sub(r'# (.*)$', r'__\1__\n', line)
+      if in_codeblock:
+        lines.append(line)
+        continue
 
-    # TODO: Parse type names in parentheses after the argument/attribute name.
-    if current_section in ('arguments', 'parameters'):
-      style = r'- __\1__:\3'
-    elif current_section in ('attributes', 'members', 'raises'):
-      style = r'- `\1`:\3'
-    elif current_section in ('returns',):
-      style = r'`\1`:\3'
-    else:
-      style = None
-    if style:
-      #                  | ident  | types     | doc
-      line = re.sub(r'\s*([^\\:]+)(\s*\(.+\))?:(.*)$', style, line)
+      if line in self._keywords_map:
+        keyword = self._keywords_map[line]
+        continue
 
-    return line, current_section
+      if keyword is None:
+        lines.append(line)
+        continue
 
-  def _preprocess_refs(self, content):
-    # TODO: Generate links to the referenced symbols.
-    def handler(match):
-      ref = match.group('ref')
-      parens = match.group('parens') or ''
-      has_trailing_dot = False
-      if not parens and ref.endswith('.'):
-        ref = ref[:-1]
-        has_trailing_dot = True
-      result = '`{}`'.format(ref + parens)
-      if has_trailing_dot:
-        result += '.'
-      return (match.group('prefix') or '') + result
-    return re.sub('(?P<prefix>^| |\t)#(?P<ref>[\w\d\._]+)(?P<parens>\(\))?', handler, content)
+      if keyword not in components:
+        components[keyword] = []
+
+      for param_re in self._param_res:
+        param_match = param_re.match(line)
+        if param_match:
+          if 'type' in param_match.groupdict():
+            components[keyword].append(
+              '- `{param}` _{type}_ - {desc}'.format(**param_match.groupdict()))
+          else:
+            components[keyword].append(
+              '- `{param}` - {desc}'.format(**param_match.groupdict()))
+          break
+
+      if not param_match:
+        components[keyword].append('  {line}'.format(line=line))
+
+    for key in components:
+      self._append_section(lines, key, components)
+
+    section.content = '\n'.join(lines)
+
+  @staticmethod
+  def _append_section(lines, key, sections):
+    section = sections.get(key)
+    if not section:
+      return
+
+    if lines and lines[-1]:
+      lines.append('')
+
+    # add an extra line because of markdown syntax
+    lines.extend(['**{}**:'.format(key), ''])
+    lines.extend(section)
