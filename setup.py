cce251d2ca59388011074124cdab325e9a5df99e
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-03-01 04:13:53 +0100

    add package.yaml for managing release and distribution of pydoc-markdown with Shore (https://git.niklasrosenstein.com/NiklasRosenstein/shore)

--- setup.py
+++ setup.py
@@ -1,65 +1,41 @@
-# Copyright (C) 2016  Niklas Rosenstein
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
 
-import setuptools
 import io
+import re
+import setuptools
+import sys
+
+with io.open('pydocmd/__init__.py', encoding='utf8') as fp:
+  version = re.search(r"__version__\s*=\s*'(.*)'", fp.read()).group(1)
 
 with io.open('README.md', encoding='utf8') as fp:
-    readme = fp.read()
+  long_description = fp.read()
+
+requirements = ['MkDocs>=1.0.0', 'Markdown>=2.6.11', 'PyYAML>=3.12', 'six>=0.11.0', 'yapf>=0.26.0']
 
 setuptools.setup(
-    name = 'pydoc-markdown',
-    version = '2.0.5',
-    description = 'Create Python API documentation in Markdown format',
-    long_description = readme,
-    long_description_content_type = 'text/markdown',
-    url = 'https://github.com/NiklasRosenstein/pydoc-markdown',
-    author = 'Niklas Rosenstein',
-    author_email = 'rosensteinniklas@gmail.com',
-    license = 'MIT',
-    classifiers = [
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'Topic :: Software Development :: Code Generators',
-        'Topic :: Utilities',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-    keywords = 'markdown pydoc generator docs documentation',
-    packages = ['pydocmd'],
-    install_requires = [
-        'MkDocs>=1.0.0',
-        'Markdown>=2.6.11',
-        'PyYAML>=3.12',
-        'six>=0.11.0',
-        'yapf>=0.26.0',
-    ],
-    entry_points = dict(
-        console_scripts = [
-            'pydocmd=pydocmd.__main__:main',
-        ],
-    )
+  name = 'pydoc-markdown',
+  version = version,
+  author = 'Niklas Rosenstein',
+  author_email = 'rosensteinniklas@gmail.com',
+  description = 'Create Python API documentation in Markdown format',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/NiklasRosenstein/pydoc-markdown',
+  license = 'MIT',
+  packages = setuptools.find_packages('.', ['test', 'test.*', 'docs', 'docs.*']),
+  package_dir = {'': '.'},
+  include_package_data = False,
+  install_requires = requirements,
+  extras_require = {},
+  tests_require = [],
+  python_requires = None, # TODO: '>=3.4,<4.0.0',
+  data_files = [],
+  entry_points = {
+    'console_scripts': [
+      'pydocmd=pydocmd.__main__:main',
+    ]
+  },
+  cmdclass = {},
+  keywords = ['markdown', 'pydoc', 'generator', 'docs', 'documentation'],
+  classifiers = ['Development Status :: 3 - Alpha', 'Intended Audience :: Developers', 'Intended Audience :: End Users/Desktop', 'Topic :: Software Development :: Code Generators', 'Topic :: Utilities', 'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 2', 'Programming Language :: Python :: 2.7', 'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.3', 'Programming Language :: Python :: 3.4', 'Programming Language :: Python :: 3.5'],
 )
