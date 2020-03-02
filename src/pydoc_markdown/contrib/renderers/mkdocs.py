19ca9c5004962c889e34c2a861d88d2f14b48673
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-03-02 02:04:11 +0100

    begin implementation of MkDocsRenderer

--- src/pydoc_markdown/contrib/renderers/mkdocs.py
+++ src/pydoc_markdown/contrib/renderers/mkdocs.py
@@ -19,23 +19,112 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-"""
-Produces MkDocs structure.
-"""
+""" Implements the `mkdocs` renderer. It uses the `markdown` renderer to
+produce an MkDocs-compatible folder structure. """
 
-
-from nr.databind.core import Field, Struct
-from nr.interface import implements
+from nr.databind.core import Field, Struct, forward_decl
+from nr.interface import implements, override
 from pydoc_markdown.contrib.renderers.markdown import MarkdownRenderer
 from pydoc_markdown.interfaces import Renderer
+from pydoc_markdown.reflection import Object, ModuleGraph
+from typing import Iterable, List, Tuple
+import copy
+import fnmatch
+import logging
 import os
+import re
+import yaml
+
+logger = logging.getLogger(__name__)
+Page = forward_decl()
+
+
+class CustomizedMarkdownRenderer(MarkdownRenderer):
+  """ We override some defaults in this subclass. """
+
+  render_toc = Field(bool, default=False)
+
+
+@forward_decl(Page)  # pylint: disable=function-redefined
+class Page(Struct):
+  """ Desribes a page that is rendered by the #MkDocsRenderer. """
+
+  name = Field(str, default=None)
+  title = Field(str)
+  source = Field(str, default=None)
+  contents = Field([str], default=None)
+  children = Field([Page], default=list)
+
+  def get_name(self):
+    if self.name:
+      return self.name
+    return re.sub(r'\s+', '-', self.title.lower())
 
 
 @implements(Renderer)
 class MkDocsRenderer(Struct):
-  output_directory = Field(str)
+  #: The output directory for the generated Markdown files. Defaults to
+  #: `build/docs`.
+  output_directory = Field(str, default='build/docs')
 
-  def render(self, modules):
-    for m in modules:
-      filename = os.path.join(self.config.output_directory, m.name + '.md')
-      print(filename)
+  #: The pages to render into the output directory.
+  pages = Field([Page])
+
+  #: Markdown renderer settings.
+  markdown = Field(CustomizedMarkdownRenderer, default=CustomizedMarkdownRenderer)
+
+  #: The name of the site. This will be carried into the `site_name` key
+  #: of the #mkdocs_config.
+  site_name = Field(str, default='My Project')
+
+  #: Arbitrary configuration values that will be rendered to an
+  #: `mkdocs.yml` file.
+  mkdocs_config = Field(dict, default=dict)
+
+  def organize_modules(self, graph) -> Iterable[Tuple[Page, List[Object]]]:
+    """ Organizes the objects in the *graph* in pairs with the pages that
+    are supposed to contain them (per the "contents" filter). """
+
+    def _match(page, node):
+      if not page.contents:
+        return False
+      path = node.path()
+      return any(fnmatch.fnmatch(path, x) for x in page.contents or ())
+
+    def _visit(page):
+      def _update_nodes(x):
+        x.visible = _match(page, x)
+      clone = copy.deepcopy(graph)
+      clone.visit(_update_nodes)
+      yield page, clone.modules
+      for sub_page in page.children:
+        yield from _visit(sub_page)
+
+    for page in self.pages:
+      yield from _visit(page)
+
+  # Renderer
+
+  @override
+  def render(self, graph):
+    docs_dir = os.path.join(self.output_directory, 'docs')
+    for page, items in self.organize_modules(graph):
+      # TODO (@NiklasRosenstein): Handle #Page.source
+      filename = os.path.join(docs_dir, page.get_name() + '.md')
+      os.makedirs(os.path.dirname(filename), exist_ok=True)
+      self.markdown.filename = filename
+      logger.info('Rendering "%s"', filename)
+      self.markdown.render(ModuleGraph(items))
+
+    config = copy.deepcopy(self.mkdocs_config)
+    config['site_name'] = self.site_name
+    config['docs_dir'] = 'docs'
+
+    filename = os.path.join(self.output_directory, 'mkdocs.yml')
+    logger.info('Rendering "%s"', filename)
+    with open(filename, 'w') as fp:
+      yaml.dump(config, fp)
+
+  @override
+  def get_resolver(self, _graph):
+    return None  # TODO
