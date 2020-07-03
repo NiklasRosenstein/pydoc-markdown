31b882d49e7e7d97d16e2bccf47c46dbfe0616b4
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-07-03 14:59:47 +0200

    mkdocs proxy now dispatches to pydoc-markdown

--- pydoc-markdown-readthedocs-mkdocs-proxy/src/pydoc_markdown_readthedocs_mkdocs_proxy/__init__.py
+++ pydoc-markdown-readthedocs-mkdocs-proxy/src/pydoc_markdown_readthedocs_mkdocs_proxy/__init__.py
@@ -14,15 +14,17 @@ def main():
   build.add_argument('--clean', action='store_true')
   build.add_argument('--site-dir')
   build.add_argument('--config-file')
-  build.add_argument('--version', action='store_true')
+  build.add_argument('--version', action='store_true',
+    help='Prints the version of the package. This is mainly used to test the hook that is '
+         'installed with this packages which catches "python -m mkdocs build".')
   args = parser.parse_args()
 
   if args.version:
     print(__name__, __version__)
     return
 
-  print('>>', args)
-  # TODO (@Niklas Rosenstein): Route Mkdocs call to Pydoc-Markdown instead.
+  # TODO (@NiklasRosenstein): Pass --clean and --config-file as well?
+  cli(['--build', '--site-dir', args.site_dir])
 
 
 if __name__ == '__main__':
