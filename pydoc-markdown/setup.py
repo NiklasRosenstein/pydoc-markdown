1743cb0b06228b97f3465ebbeef06e2b9a768fb2
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-10-27 21:29:12 +0100

    add "git" source linker, re-arrange inheritance for source linkers

--- pydoc-markdown/setup.py
+++ pydoc-markdown/setup.py
@@ -75,9 +75,9 @@ setuptools.setup(
       'docusaurus = pydoc_markdown.contrib.renderers.docusaurus:DocusaurusRenderer',
     ],
     'pydoc_markdown.interfaces.SourceLinker': [
-      'github = pydoc_markdown.contrib.source_linkers.basegit:GithubSourceLinker',
-      'gitea = pydoc_markdown.contrib.source_linkers.basegit:GiteaSourceLinker',
-
+      'git = pydoc_markdown.contrib.source_linkers.git:GitSourceLinker',
+      'github = pydoc_markdown.contrib.source_linkers.git:GithubSourceLinker',
+      'gitea = pydoc_markdown.contrib.source_linkers.git:GiteaSourceLinker',
     ]
   },
   cmdclass = {},
