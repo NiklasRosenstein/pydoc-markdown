e6e2a8f4cd521a4756ed1948d11e3c390f19f46c
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-10-28 21:03:09 +0100

    (pydoc-markdown) bump version to 3.6.0

--- pydoc-markdown/setup.py
+++ pydoc-markdown/setup.py
@@ -37,7 +37,7 @@ requirements = [
 
 setuptools.setup(
   name = 'pydoc-markdown',
-  version = '3.5.0',
+  version = '3.6.0',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'Create Python API documentation in Markdown format.',
