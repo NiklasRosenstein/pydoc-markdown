124af6408d2593d258f3466516c5f06bf1739318
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-02-20 22:47:18 +0100

    (pydoc-markdown) bump version to 3.10.0

--- pydoc-markdown/src/pydoc_markdown/__init__.py
+++ pydoc-markdown/src/pydoc_markdown/__init__.py
@@ -43,7 +43,7 @@ import subprocess
 import yaml
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '3.9.0'
+__version__ = '3.10.0'
 
 mapper = ObjectMapper(JsonModule())
 logger = logging.getLogger(__name__)
