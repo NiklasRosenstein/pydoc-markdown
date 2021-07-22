7cbfc1855fe7c55d76bad5820826b90882c710a1
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-07-23 00:23:35 +0200

    feature: add `blockquote` and `describe` built-in filters

--- pydoc-markdown/src/pydoc_markdown/contrib/renderers/jinja2.py
+++ pydoc-markdown/src/pydoc_markdown/contrib/renderers/jinja2.py
@@ -11,7 +11,7 @@ import jinja2
 
 from pydoc_markdown.contrib.renderers.markdown import MarkdownReferenceResolver
 from pydoc_markdown.interfaces import Renderer, Resolver
-from pydoc_markdown.util.docspec import format_function_signature, get_members_of_type
+from pydoc_markdown.util.docspec import format_function_signature, get_members_of_type, get_object_description
 
 T = t.TypeVar('T')
 log = logging.getLogger(__name__)
@@ -61,7 +61,7 @@ class Jinja2Renderer(Renderer):
 
     for render in self.renders:
       env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'), **render.jinja2_environment_settings)
-      setup_env(env)
+      setup_env(env, resolver.reverse_map)
       env.filters['uid'] = resolver.generate_object_id
       template = env.get_template(render.template)
       for filename, args in render.produces.items():
@@ -74,23 +74,29 @@ class Jinja2Renderer(Renderer):
     return MarkdownReferenceResolver(modules)
 
 
-def setup_env(env: jinja2.Environment) -> None:
+def setup_env(env: jinja2.Environment, reverse_map: docspec.ReverseMap) -> None:
   env.filters['classes'] = lambda modules: get_members_of_type(modules, docspec.Class)
   env.filters['functions'] = lambda objs: get_members_of_type(objs, docspec.Function)
   env.filters['attrs'] = lambda objs: get_members_of_type(objs, docspec.Data)
   env.filters['indent'] = _indent_filter
+  env.filters['blockquote'] = _blockquote_filter
   env.filters['first_line'] = _first_line_filter
   env.filters['format_arglist'] = format_arglist
   env.filters['format_function_signature'] = format_function_signature
+  env.filters['describe'] = lambda obj: get_object_description(obj, reverse_map)
 
 
-def _indent_filter(text: str, level: int = 1) -> str:
+def _indent_filter(text: t.Optional[str], level: int = 1, ) -> str:
   if not text: return ''
   lines = text.splitlines()
   return '\n'.join(lines[:1] + list('    ' * level + l for l in lines[1:]))
 
 
-def _first_line_filter(text: str) -> str:
-  if not text:
-    return ''
+def _blockquote_filter(text: t.Optional[str]) -> str:
+  if not text: return ''
+  return '> ' + '\n> '.join(text.splitlines())
+
+
+def _first_line_filter(text: t.Optional[str]) -> str:
+  if not text: return ''
   return text.splitlines()[0]
--- pydoc-markdown/src/pydoc_markdown/util/docspec.py
+++ pydoc-markdown/src/pydoc_markdown/util/docspec.py
@@ -1,5 +1,7 @@
 
+from abc import abstractproperty
 import typing as t
+import typing_extensions as te
 
 import docspec
 import docspec_python
@@ -28,5 +30,56 @@ def format_function_signature(func: docspec.Function, exclude_self: bool = False
   return sig
 
 
-def is_method(obj: docspec.ApiObject, reverse_map: docspec.ReverseMap) -> bool:
-  return isinstance(obj, docspec.Function) and isinstance(reverse_map.get_parent(obj), docspec.Class)
+def is_function(obj: docspec.ApiObject) -> te.TypeGuard[docspec.Function]:
+  return isinstance(obj, docspec.Function)
+
+
+def is_method(obj: docspec.ApiObject, reverse_map: docspec.ReverseMap) -> te.TypeGuard[docspec.Function]:
+  return is_function(obj) and isinstance(reverse_map.get_parent(obj), docspec.Class)
+
+
+def is_property(obj: docspec.ApiObject) -> te.TypeGuard[docspec.Function]:
+  return is_function(obj) and any(d.name == 'property' for d in obj.decorations)
+
+
+def is_attr(obj: docspec.ApiObject, reverse_map: docspec.ReverseMap) -> te.TypeGuard[docspec.Data]:
+  return isinstance(obj, docspec.Data) and isinstance(reverse_map.get_parent(obj), docspec.Class)
+
+
+def get_object_description(obj: docspec.ApiObject, reverse_map: docspec.ReverseMap = None) -> str:
+  """
+  Determines the best matching description of *obj*. Requires a #docspec.ReverseMap such that
+  methods can be correclty identified as such. Returns a string such as "module", "class", "abstract class",
+  "enum", "attribute", "property", "function", "method", "abstract method", "class method", "static method"
+  and "abstract class method".
+  """
+
+  if isinstance(obj, docspec.Module):
+    return 'module'
+  elif isinstance(obj, docspec.Class):
+    if any(x in (obj.bases or ()) for x in ('abc.ABC', 'ABC')) or \
+       any(x in (obj.metaclass or ()) for x in ('abc.ABCMeta', 'ABCMeta')):
+      return 'abstract class'
+    return 'class'
+  elif isinstance(obj, docspec.Function):
+    if any(x.name == 'property' for x in obj.decorations):
+      return 'property'
+    if any(x.name in ('abc.abstractproperty', 'abstractproperty') for x in obj.decorations):
+      return 'abstract property'
+    if any(x.name in ('abc.abstractmethod', 'abstractmethod') for x in obj.decorations):
+      return 'abstract method'
+    if any(x.name in ('abc.abstractclassmethod', 'abstractclassmethod') for x in obj.decorations):
+      return 'abstract class method'
+    if any(x.name == 'classmethod' for x in obj.decorations):
+      return 'class method'
+    if any(x.name == 'staticmethod' for x in obj.decorations):
+      return 'static method'
+    if reverse_map and is_method(obj, reverse_map):
+      return 'method'
+    return 'function'
+  elif isinstance(obj, docspec.Data):
+    if reverse_map and is_attr(obj, reverse_map):
+      return 'attribute'
+    return 'data'
+  else:
+    assert False, type(obj)
