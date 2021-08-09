e8dc4ef5f9d687933dfde8b33528c3653b8ec60f
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-08-09 04:26:22 +0200

    (pydoc-markdown) bump version to 4.1.6

--- pydoc-markdown/setup.py
+++ pydoc-markdown/setup.py
@@ -46,7 +46,7 @@ requirements = [
 
 setuptools.setup(
   name = 'pydoc-markdown',
-  version = '4.1.5',
+  version = '4.1.6',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'Create Python API documentation in Markdown format.',
--- pydoc-markdown/src/pydoc_markdown/__init__.py
+++ pydoc-markdown/src/pydoc_markdown/__init__.py
@@ -45,7 +45,7 @@ from pydoc_markdown.util import ytemplate
 
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '4.1.5'
+__version__ = '4.1.6'
 
 logger = logging.getLogger(__name__)
 
