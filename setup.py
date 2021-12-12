1ecf649578659d5cb24fcb558bf70e97796596dd
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-12-12 20:28:07 +0900

    (pydoc-markdown) bump version to 4.4.0

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ requirements = [
 
 setuptools.setup(
   name = 'pydoc-markdown',
-  version = '4.3.2',
+  version = '4.4.0',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'Create Python API documentation in Markdown format.',
