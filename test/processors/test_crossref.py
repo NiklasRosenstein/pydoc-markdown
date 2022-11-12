5f6166847b11bbcad5ab6e4f1b2b315dd9a3fbc5
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-11-12 23:26:40 +0000

    fix fmt

--- test/processors/test_crossref.py
+++ test/processors/test_crossref.py
@@ -1,7 +1,8 @@
-
 from pydoc_markdown.contrib.processors.crossref import CrossrefProcessor
 
 from . import assert_processor_result
+
+
 def test__CrossrefProcessor__does_not_disrupt_local_markdown_anchor_references() -> None:
     assert_processor_result(
         CrossrefProcessor(),
