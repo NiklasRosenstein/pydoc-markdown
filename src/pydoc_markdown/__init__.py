a9bb4b3a0f82ecdec072e8ccd4109bb6f5d7f9c2
Niklas Rosenstein <rosensteinniklas@gmail.com>
2023-05-28 02:49:02 +0000

    release 4.7.0

--- src/pydoc_markdown/__init__.py
+++ src/pydoc_markdown/__init__.py
@@ -45,7 +45,7 @@ from pydoc_markdown.interfaces import Builder, Context, Loader, Processor, Rende
 from pydoc_markdown.util import ytemplate
 
 __author__ = "Niklas Rosenstein <rosensteinniklas@gmail.com>"
-__version__ = "4.6.4"
+__version__ = "4.7.0"
 
 logger = logging.getLogger(__name__)
 
