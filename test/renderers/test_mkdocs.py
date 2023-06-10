d57a36ef75aef0678ada36975bb6029fa31d4027
Niklas Rosenstein <rosensteinniklas@gmail.com>
2023-06-10 10:15:02 +0100

    Add unit test for deserializing mkdocs config

---
+++ test/renderers/test_mkdocs.py
@@ -0,0 +1,26 @@
+from databind.json import load
+
+from pydoc_markdown.contrib.renderers.mkdocs import MkdocsRenderer
+from pydoc_markdown.util.pages import Page
+
+
+def test_deserialize_mkdocs_renderer() -> None:
+    payload = {
+        "pages": [
+            {
+                "title": "Home",
+                "name": "index",
+                "source": "README.md",
+            }
+        ]
+    }
+    renderer = load(payload, MkdocsRenderer)
+    assert renderer == MkdocsRenderer(
+        pages=[
+            Page(
+                title="Home",
+                name="index",
+                source="README.md",
+            ),
+        ]
+    )
